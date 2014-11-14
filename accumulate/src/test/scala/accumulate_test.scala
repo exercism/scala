31849935cbdbcaa91a3059eb6a381559263e76fa
Ric Emery <ric.emery@gmail.com>
2014-11-13 21:05:14 -0700

    Add accumulate exercise

---
+++ accumulate/src/test/scala/accumulate_test.scala
@@ -0,0 +1,34 @@
+import org.scalatest.{Matchers, FlatSpec}
+
+class AccumulateTest extends FlatSpec with Matchers {
+
+  it should "allow empty accumulation" in {
+    val accumulate = new Accumulate
+    val accumulation = accumulate.accumulate[Int, Int](x => x * x, List.empty)
+    accumulation should be (List.empty)
+  }
+
+  it should "accumulate squares" in {
+    val accumulate = new Accumulate
+    val accumulation = accumulate.accumulate[Int, Int](x => x * x, List(1, 2, 3))
+    accumulation should be (List(1, 4, 9))
+  }
+
+  it should "accumulate upcases" in {
+    val accumulate = new Accumulate
+    val accumulation = accumulate.accumulate[String, String](_.map(_.toUpper), List("hello", "world"))
+    accumulation should be (List("HELLO", "WORLD"))
+  }
+
+  it should "accumulate reversed strings" in {
+    val accumulate = new Accumulate
+    val accumulation = accumulate.accumulate[String, String](_.reverse, List("eht", "kciuq", "nworb", "xof", "cte"))
+    accumulation should be (List("the", "quick", "brown", "fox", "etc"))
+  }
+
+  it should "allow different return type" in {
+    val accumulate = new Accumulate
+    val accumulation = accumulate.accumulate[Int, String](_.toString, List(1, 2, 3))
+    accumulation should be (List("1", "2", "3"))
+  }
+}
