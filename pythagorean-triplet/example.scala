3d07b2d0ccbc5b1ee4ddfab4729c95a4fc331db1
Ric Emery <ric.emery@gmail.com>
2015-03-30 17:11:38 -0700

    add pythagorean-triplet exercise

---
+++ pythagorean-triplet/example.scala
@@ -0,0 +1,22 @@
+object PythagoreanTriplet {
+  private def sqr(x: Int) = x * x
+
+  private def sort(triplet: (Int, Int, Int)): (Int, Int, Int) =
+    triplet match {
+      case (a, b, c) if b < a => sort((b, a, c))
+      case (a, b, c) if c < b => sort((a, c, b))
+      case (a, b, c) => triplet
+    }
+
+  def isPythagorean(triplet: (Int, Int, Int)): Boolean = {
+    val (a, b, c) = sort(triplet)
+    sqr(a) + sqr(b) == sqr(c)
+  }
+
+  def pythagoreanTriplets(minFactor: Int, maxFactor:Int): Seq[(Int, Int, Int)] =
+    for {a <- minFactor to maxFactor
+         b <- a to maxFactor
+         c2 = sqr(a) + sqr(b)
+         c = Math.sqrt(c2).asInstanceOf[Int]
+         if c <= maxFactor && c2 == sqr(c)} yield (a, b, c)
+}
