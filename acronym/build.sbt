f2dd30b26bfb8dac7027f2ce00912dcc0e269a21
Ric Emery <ric.emery@gmail.com>
2015-12-12 15:39:40 -0700

    Add acronym exercise

---
+++ acronym/build.sbt
@@ -0,0 +1,3 @@
+scalaVersion := "2.11.7"
+
+libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.5" % "test"
---
+++ acronym/src/test/scala/AcronymTest.scala
@@ -0,0 +1,15 @@
+import org.scalatest.{FlatSpec, Matchers}
+
+class AcronymTest extends FlatSpec with Matchers {
+  private val acronyms = List(
+    ("Portable Network Graphics", "PNG"),
+    ("Ruby on Rails", "ROR"),
+    ("HyperText Markup Language", "HTML"),
+    ("First In, First Out",  "FIFO"),
+    ("PHP: Hypertext Preprocessor", "PHP"),
+    ("Complementary metal-oxide semiconductor", "CMOS"))
+
+  it should "create acronyms" in {
+    acronyms.foreach{case (phrase, acronym) => Acroynm(phrase).abberviate should equal(acronym)}
+  }
+}
