f267eb794251b36053aa612fe1dee1fbb8cdb0ca
Ric Emery <ric.emery@gmail.com>
2016-09-05 12:53:17 -0700

    Deprecate binary, hexadecimal, octal and trinary. Add all-your-base

---
+++ exercises/all-your-base/example.scala
@@ -0,0 +1,33 @@
+import scala.annotation.tailrec
+
+object AllYourBase {
+  def rebase(inputBase: Int, inputDigits: List[Int], outputBase: Int): Option[List[Int]] = {
+    if (inputBase < 2 || outputBase < 2 || inputDigits.isEmpty)
+      None
+    else {
+      fromDigits(0, inputBase, inputDigits) match {
+        case None => None
+        case Some(x) => Some(toDigits(outputBase, x, List()))
+      }
+    }
+  }
+
+  @tailrec
+  private def fromDigits(acc: Int, base: Int, digits: List[Int]): Option[Int] = {
+    digits match {
+      case x::xs => if (x >= 0 && x < base)
+                      fromDigits(acc * base + x, base, xs)
+                    else
+                      None
+      case Nil => Option(acc)
+    }
+  }
+
+  @tailrec
+  private def toDigits(base: Int, x: Int, acc: List[Int]): List[Int] = {
+    x match {
+      case 0 => acc
+      case _ => toDigits(base, x / base, x % base :: acc)
+    }
+  }
+}
