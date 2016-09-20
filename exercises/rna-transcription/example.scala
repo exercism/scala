5c814f7f141154408bf55e2fa77234877433990b
abo64 <abo64@gmx.de>
2016-09-20 16:45:39 +0200

    changed signature of toRna to return Option

--- exercises/rna-transcription/example.scala
+++ exercises/rna-transcription/example.scala
@@ -1,15 +1,20 @@
-class Dna {
-
-  val dnaToRna = Map(
-    'G' -> 'C',
-    'C' -> 'G',
-    'T' -> 'A',
-    'A' -> 'U'
-  )
-
-  def toRna(dna: String) = dna.map(dnaToRna)
-}
+import Dna._
 
 object Dna {
-  def apply() = new Dna()
+  private type Strand = String
+  private type Nucleotide = Char
+
+  def toRna(dna: Strand): Option[Strand] =
+    dna.foldRight (Option("")) { case (char, rna) =>
+      for {
+        rnaNucleotide <- toRnaNucleotide(char)
+        rnaStrand <- rna
+      } yield rnaNucleotide +: rnaStrand
+  }
+
+  private val DnaToRna =
+    Map('G' -> 'C', 'C' -> 'G', 'T' -> 'A', 'A' -> 'U')
+
+  private def toRnaNucleotide(char: Char): Option[Nucleotide] =
+     DnaToRna get char
 }
--- exercises/rna-transcription/src/test/scala/transcription_test.scala
+++ exercises/rna-transcription/src/test/scala/transcription_test.scala
@@ -2,26 +2,39 @@ import org.scalatest.{Matchers, FunSuite}
 
 class DnaTest extends FunSuite with Matchers {
   test("transcribes blank string") {
-    Dna().toRna("") should be ("")
+    Dna.toRna("") should be (Some(""))
   }
 
-  test("transcribes cytidine to guanosine") {
-    Dna().toRna("C") should be ("G")
+  test("transcribes cytidine to guanine") {
+    Dna.toRna("C") should be (Some("G"))
   }
 
-  test("transcribes guanosine to cytidine") {
-    Dna().toRna("G") should be ("C")
+  test("transcribes guanosine to cytosine") {
+    Dna.toRna("G") should be (Some("C"))
   }
 
   test("transcribes adenosine to uracil") {
-    Dna().toRna("A") should be ("U")
+    Dna.toRna("A") should be (Some("U"))
   }
 
-  test("transcribes thymidine to adenosine") {
-    Dna().toRna("T") should be ("A")
+  test("transcribes thymidine to adenine") {
+    Dna.toRna("T") should be (Some("A"))
   }
 
   test("transcribes all ACGT to UGCA") {
-    Dna().toRna("ACGTGGTCTTAA") should be ("UGCACCAGAAUU")
+    Dna.toRna("ACGTGGTCTTAA") should be (Some("UGCACCAGAAUU"))
+  }
+
+  test("transcribes RNA only nucleotide uracil to None") {
+    Dna.toRna("U") should be (None)
+  }
+
+  test("transcribes completely invalid DNA to None") {
+    Dna.toRna("XXX") should be (None)
+  }
+
+  test("transcribes partially invalid DNA to None") {
+    Dna.toRna("ACGTXXXCTTAA") should be (None)
   }
 }
+
