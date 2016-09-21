6c9e1d8ade7091073103cebd41468960f09d1568
abo64 <abo64@gmx.de>
2016-09-21 07:13:10 +0200

    changed signature of square to return Option

--- exercises/grains/example.scala
+++ exercises/grains/example.scala
@@ -1,4 +1,29 @@
+import scala.annotation.tailrec
+
 object Grains {
-  def square(x: Int): BigInt = BigInt(2).pow(x-1)
-  def total = 1.to(64).map(square).sum
+  type Grains = BigInt
+  type ChessboardSquare = Int
+
+  private val ChessboardSquares = 64
+
+  def square(cbSquare: ChessboardSquare): Option[Grains] = {
+    def isValidChessboardSquare(n: Int) =
+      1 <= n && n <= ChessboardSquares
+
+    def sq(n: Int): BigInt = {
+      @tailrec
+      def loop(n: Int, acc: BigInt): BigInt =
+        if (n == 0) acc
+        else loop(n - 1, acc * 2)
+
+      loop(n, 1)
+    }
+
+    val pred = (_:Int) - 1
+
+    Option(cbSquare) filter isValidChessboardSquare map pred.andThen(sq)
+  }
+
+  def total: Grains =
+    (1 to ChessboardSquares) flatMap (square _).andThen(_.toList) sum
 }
