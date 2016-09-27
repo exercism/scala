2a8737c072c680badaa57ecca8f8fd76ed6c1fcf
Tobias Frischholz <tfrischholz@kabelmail.de>
2016-09-27 23:26:36 +0200

    Improved "bank-account" example * Make sure we close redundancies * Make sure we using the features of case classes * Make sure we using flatMap as functional approach (The monadic version is prefered by the scala guys)

--- exercises/bank-account/example.scala
+++ exercises/bank-account/example.scala
@@ -1,37 +1,30 @@
 trait BankAccount {
+
   def closeAccount(): Unit
+
   def getBalance: Option[Int]
+
   def incrementBalance(increment: Int): Option[Int]
 }
 
-protected case class Account() extends BankAccount {
-  private var balance: Option[Int] = Some(0)
+protected case class Account(var balance: Option[Int] = Some(0)) extends BankAccount {
 
-  override def closeAccount(): Unit = {
-    this.synchronized {
-      balance = None
-    }
-  }
+  private def runThreadSafe[A](block: => A): A = this.synchronized(block)
 
-  override def getBalance: Option[Int] = {
-    this.synchronized {
+  override def closeAccount(): Unit = runThreadSafe(balance = None)
+
+  override def getBalance: Option[Int] = runThreadSafe(balance)
+
+  override def incrementBalance(increment: Int): Option[Int] = runThreadSafe {
+    balance flatMap { amount =>
+      balance = Some(amount + increment)
       balance
-    }
-  }
-
-  override def incrementBalance(increment: Int): Option[Int] = {
-    this.synchronized {
-      balance match {
-        case Some(b) => balance = Some(b + increment)
-          balance
-        case None => None
-      }
     }
   }
 }
 
 object BankAccount {
-  def apply(): BankAccount = new Account()
+  def apply(): BankAccount = Account()
 }
 
 
