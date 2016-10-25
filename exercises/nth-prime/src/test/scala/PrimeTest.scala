33125fc47ff75809ceade84588dbf68afe3b6a83
Erik Schierboom <erik_schierboom@hotmail.com>
2016-10-25 20:56:56 +0200

    Added pending keyword to nth-prime test
    Work on #133 (@Trevoke)

--- exercises/nth-prime/src/test/scala/PrimeTest.scala
+++ exercises/nth-prime/src/test/scala/PrimeTest.scala
@@ -6,22 +6,27 @@ class PrimeTest extends FlatSpec with Matchers {
   }
 
   it should "calculate 2nd prime" in {
+    pending
     Prime.nth(2) should be (3)
   }
 
   it should "calculate 6th prime" in {
+    pending
     Prime.nth(6) should be (13)
   }
 
   it should "calculate 1000th prime" in {
+    pending
     Prime.nth(1000) should be (7919)
   }
 
   it should "calculate 10000th prime" in {
+    pending
     Prime.nth(10000) should be (104729)
   }
 
   it should "calculate 10001th prime" in {
+    pending
     Prime.nth(10001) should be (104743)
   }
 }
