326603b47b7d05b5d0c7a0493fa5d08eb5fda580
Erik Schierboom <erik_schierboom@hotmail.com>
2016-10-25 20:18:04 +0200

    Added pending keyword to scrabble-score test
    Work on #133

--- exercises/scrabble-score/src/test/scala/scrabble_score_test.scala
+++ exercises/scrabble-score/src/test/scala/scrabble_score_test.scala
@@ -6,26 +6,32 @@ class scrabble_score_test extends FunSuite with Matchers {
   }
 
   test("score letter uppercase") {
+    pending
     new Scrabble().scoreLetter('A') should be (1)
   }
 
   test("score word") {
+    pending
     new Scrabble().scoreWord("at") should be (2)
   }
 
   test("score word with dupe letters") {
+    pending
     new Scrabble().scoreWord("street") should be (6)
   }
 
   test("score quirky word") {
+    pending
     new Scrabble().scoreWord("quirky") should be (22)
   }
 
   test("score capitalized word") {
+    pending
     new Scrabble().scoreWord("OXYPHENBUTAZONE") should be (41)
   }
 
   test("score mixed case word") {
+    pending
     new Scrabble().scoreWord("Oxyphenbutazone") should be (41)
   }
 }
