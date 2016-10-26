c32be02405ea49abb920c1a63990fd305bcf4165
Erik Schierboom <erik_schierboom@hotmail.com>
2016-10-26 19:30:42 +0200

    Added pending keyword to pascals-triangle test
    Work on #133 (@Trevoke)

--- exercises/pascals-triangle/src/test/scala/PascalsTriangleTest.scala
+++ exercises/pascals-triangle/src/test/scala/PascalsTriangleTest.scala
@@ -6,24 +6,29 @@ class PascalsTriangleTest extends FlatSpec with Matchers {
   }
 
   it should "handle 2 rows" in {
+    pending
     PascalsTriangle.triangle(2) should be (List(List(1), List(1, 1)))
   }
 
   it should "handle 3 rows" in {
+    pending
     PascalsTriangle.triangle(3) should be (List(List(1), List(1, 1), List(1, 2, 1)))
   }
 
   it should "handle 4 rows" in {
+    pending
     PascalsTriangle.triangle(4) should be (List(List(1), List(1, 1),
       List(1, 2, 1), List(1, 3, 3, 1)))
   }
 
   it should "handle 5 rows" in {
+    pending
     PascalsTriangle.triangle(5) should be (List(List(1), List(1, 1),
       List(1, 2, 1), List(1, 3, 3, 1), List(1, 4, 6, 4, 1)))
   }
 
   it should "handle 20 rows" in {
+    pending
     PascalsTriangle.triangle(20).takeRight(1).head should be (List(1, 19,
       171, 969, 3876, 11628, 27132, 50388, 75582, 92378,
       92378, 75582, 50388, 27132, 11628, 3876, 969, 171,
