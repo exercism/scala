8f9c76a78222d814ac97a27cda429c4750358c10
Erik Schierboom <erik_schierboom@hotmail.com>
2016-12-13 19:40:44 +0100

    Added pending keyword to zipper test
    Work on #133

--- exercises/zipper/src/test/scala/ZipperTest.scala
+++ exercises/zipper/src/test/scala/ZipperTest.scala
@@ -23,31 +23,38 @@ class ZipperTest extends FunSuite with Matchers {
   }
 
   test("left, right and value") {
+    pending
     z.value(fromSome(z.right(fromSome(z.left(z.fromTree(t1)))))) should be (3)
   }
 
   test("dead end") {
+    pending
     (z.left(fromSome(z.left(z.fromTree(t1))))) should be (None)
   }
 
   test("tree from deep focus") {
+    pending
     z.toTree(fromSome(z.right(fromSome(z.left(z.fromTree(t1)))))) should be (t1)
   }
 
   test("setValue") {
+    pending
     z.toTree(z.setValue(5, (fromSome(z.left(z.fromTree(t1)))))) should be (t2)
   }
 
   test("setLeft with Some") {
+    pending
     z.toTree(z.setLeft(Some(BinTree(5, None, None)),
         (fromSome(z.left(z.fromTree(t1)))))) should be (t3)
   }
 
   test("setRight with None") {
+    pending
     z.toTree(z.setRight(None, (fromSome(z.left(z.fromTree(t1)))))) should be (t4)
   }
 
   test("different paths to same zipper") {
+    pending
     z.right(fromSome(z.up(fromSome(z.left(z.fromTree(t1)))))) should be
       (z.right(z.fromTree(t1)))
   }
