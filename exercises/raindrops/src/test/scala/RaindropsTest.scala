81bed09f6c158b8365c6c9c10ab310a93c4f5fe0
Ric Emery <ric.emery@gmail.com>
2016-12-15 10:45:13 -0700

    Raindrops - use object instead of an instance - refs #242. Add Raindrops.scala - refs #137. Add topics to config.json - refs #125

--- exercises/raindrops/src/test/scala/RaindropsTest.scala
+++ exercises/raindrops/src/test/scala/RaindropsTest.scala
@@ -2,82 +2,82 @@ import org.scalatest.{Matchers, FlatSpec}
 
 class RaindropsTest extends FlatSpec with Matchers {
   it should "convert 1" in {
-    Raindrops().convert(1) should equal("1")
+    Raindrops.convert(1) should equal("1")
   }
 
   it should "convert 3" in {
     pending
-    Raindrops().convert(3) should equal("Pling")
+    Raindrops.convert(3) should equal("Pling")
   }
 
   it should "convert 5" in {
     pending
-    Raindrops().convert(5) should equal("Plang")
+    Raindrops.convert(5) should equal("Plang")
   }
 
   it should "convert 7" in {
     pending
-    Raindrops().convert(7) should equal("Plong")
+    Raindrops.convert(7) should equal("Plong")
   }
 
   it should "convert 6" in {
     pending
-    Raindrops().convert(6) should equal("Pling")
+    Raindrops.convert(6) should equal("Pling")
   }
 
   it should "convert 9" in {
     pending
-    Raindrops().convert(9) should equal("Pling")
+    Raindrops.convert(9) should equal("Pling")
   }
 
   it should "convert 10" in {
     pending
-    Raindrops().convert(10) should equal("Plang")
+    Raindrops.convert(10) should equal("Plang")
   }
 
   it should "convert 14" in {
     pending
-    Raindrops().convert(14) should equal("Plong")
+    Raindrops.convert(14) should equal("Plong")
   }
 
   it should "convert 15" in {
     pending
-    Raindrops().convert(15) should equal("PlingPlang")
+    Raindrops.convert(15) should equal("PlingPlang")
   }
 
   it should "convert 21" in {
     pending
-    Raindrops().convert(21) should equal("PlingPlong")
+    Raindrops.convert(21) should equal("PlingPlong")
   }
 
   it should "convert 25" in {
     pending
-    Raindrops().convert(25) should equal("Plang")
+    Raindrops.convert(25) should equal("Plang")
   }
 
   it should "convert 35" in {
     pending
-    Raindrops().convert(35) should equal("PlangPlong")
+    Raindrops.convert(35) should equal("PlangPlong")
   }
 
   it should "convert 49" in {
     pending
-    Raindrops().convert(49) should equal("Plong")
+    Raindrops.convert(49) should equal("Plong")
   }
 
   it should "convert 52" in {
     pending
-    Raindrops().convert(52) should equal("52")
+    Raindrops.convert(52) should equal("52")
   }
 
   it should "convert 105" in {
     pending
-    Raindrops().convert(105) should equal("PlingPlangPlong")
+    Raindrops.convert(105) should equal("PlingPlangPlong")
   }
 
   it should "convert 12121" in {
     pending
-    Raindrops().convert(12121) should equal("12121")
+    Raindrops.convert(12121) should equal("12121")
   }
 }
 
