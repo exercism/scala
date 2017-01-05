5e4bfd2dc50e780000cd753e7ba88f5097091b31
abo64 <abo64@gmx.de>
2017-01-05 11:36:54 +0100

    roman-numerals: Change class to object (issue #242)

--- exercises/roman-numerals/src/test/scala/RomanNumeralsTest.scala
+++ exercises/roman-numerals/src/test/scala/RomanNumeralsTest.scala
@@ -3,91 +3,91 @@ import org.scalatest._
 class RomanNumeralsSpecs extends FunSpec with Matchers {
 
   it ("0 equals empty string") {
-    RomanNumeral(0).value should be ("")
+    RomanNumeral.toNumerals(0) should be ("")
   }
 
   it ("1 equals I") {
     pending
-    RomanNumeral(1).value should be ("I")
+    RomanNumeral.toNumerals(1) should be ("I")
   }
 
   it ("2 equals II") {
     pending
-    RomanNumeral(2).value should be ("II")
+    RomanNumeral.toNumerals(2) should be ("II")
   }
 
   it ("3 equals III") {
     pending
-    RomanNumeral(3).value should be ("III")
+    RomanNumeral.toNumerals(3) should be ("III")
   }
 
   it ("4 equals IV") {
     pending
-    RomanNumeral(4).value should be ("IV")
+    RomanNumeral.toNumerals(4) should be ("IV")
   }
 
   it ("5 equals V") {
     pending
-    RomanNumeral(5).value should be ("V")
+    RomanNumeral.toNumerals(5) should be ("V")
   }
 
   it ("6 equals VI") {
     pending
-    RomanNumeral(6).value should be ("VI")
+    RomanNumeral.toNumerals(6) should be ("VI")
   }
 
   it ("9 equals IX") {
     pending
-    RomanNumeral(9).value should be ("IX")
+    RomanNumeral.toNumerals(9) should be ("IX")
   }
 
   it ("27 equals XXVII") {
     pending
-    RomanNumeral(27).value should be ("XXVII")
+    RomanNumeral.toNumerals(27) should be ("XXVII")
   }
 
   it ("48 equals XLVIII") {
     pending
-    RomanNumeral(48).value should be ("XLVIII")
+    RomanNumeral.toNumerals(48) should be ("XLVIII")
   }
 
   it ("59 equals LIX") {
     pending
-    RomanNumeral(59).value should be ("LIX")
+    RomanNumeral.toNumerals(59) should be ("LIX")
   }
 
   it ("93 equals XCIII") {
     pending
-    RomanNumeral(93).value should be ("XCIII")
+    RomanNumeral.toNumerals(93) should be ("XCIII")
   }
 
   it ("141 equals CXLI") {
     pending
-    RomanNumeral(141).value should be ("CXLI")
+    RomanNumeral.toNumerals(141) should be ("CXLI")
   }
 
   it ("402 equals CDII") {
     pending
-    RomanNumeral(402).value should be ("CDII")
+    RomanNumeral.toNumerals(402) should be ("CDII")
   }
 
   it ("575 equals DLXXV") {
     pending
-    RomanNumeral(575).value should be ("DLXXV")
+    RomanNumeral.toNumerals(575) should be ("DLXXV")
   }
 
   it ("911 equals CMXI") {
     pending
-    RomanNumeral(911).value should be ("CMXI")
+    RomanNumeral.toNumerals(911) should be ("CMXI")
   }
 
   it ("1024 equals MXXIV") {
     pending
-    RomanNumeral(1024).value should be ("MXXIV")
+    RomanNumeral.toNumerals(1024) should be ("MXXIV")
   }
 
   it ("3000 equals MMM") {
     pending
-    RomanNumeral(3000).value should be ("MMM")
+    RomanNumeral.toNumerals(3000) should be ("MMM")
   }
 }
