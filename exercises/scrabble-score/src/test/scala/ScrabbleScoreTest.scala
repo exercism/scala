4458be2fb44f11868fa014fd2d764650c34f5872
Ric Emery <ric.emery@gmail.com>
2017-01-23 23:19:26 -0700

    Difference of Squares - Update to Scala 2.12.1 - refs #235. Add topics to config.json - refs #125. Change solution to use object instead of class. (#305)

--- exercises/scrabble-score/src/test/scala/ScrabbleScoreTest.scala
+++ exercises/scrabble-score/src/test/scala/ScrabbleScoreTest.scala
@@ -2,37 +2,37 @@ import org.scalatest.{Matchers, FunSuite}
 
 class scrabble_score_test extends FunSuite with Matchers {
   test("score letter lowercase") {
-    new Scrabble().scoreLetter('a') should be (1)
+    Scrabble.scoreLetter('a') should be (1)
   }
 
   test("score letter uppercase") {
     pending
-    new Scrabble().scoreLetter('A') should be (1)
+    Scrabble.scoreLetter('A') should be (1)
   }
 
   test("score word") {
     pending
-    new Scrabble().scoreWord("at") should be (2)
+    Scrabble.scoreWord("at") should be (2)
   }
 
   test("score word with dupe letters") {
     pending
-    new Scrabble().scoreWord("street") should be (6)
+    Scrabble.scoreWord("street") should be (6)
   }
 
   test("score quirky word") {
     pending
-    new Scrabble().scoreWord("quirky") should be (22)
+    Scrabble.scoreWord("quirky") should be (22)
   }
 
   test("score capitalized word") {
     pending
-    new Scrabble().scoreWord("OXYPHENBUTAZONE") should be (41)
+    Scrabble.scoreWord("OXYPHENBUTAZONE") should be (41)
   }
 
   test("score mixed case word") {
     pending
-    new Scrabble().scoreWord("Oxyphenbutazone") should be (41)
+    Scrabble.scoreWord("Oxyphenbutazone") should be (41)
   }
 }
 
