7775c846e0952cc97bbbd676e88930a9444a033b
Ric Emery <ric.emery@gmail.com>
2017-02-06 11:31:19 -0700

    Add strain exercise

---
+++ exercises/strain/src/test/scala/StrainTest.scala
@@ -0,0 +1,68 @@
+import org.scalatest.{Matchers, FlatSpec}
+
+class StrainTest extends FlatSpec with Matchers {
+
+  it should "handle empty keep" in {
+    val result = Strain.keep[Int](List(), x => x < 10)
+    result should be(List.empty)
+  }
+
+  it should "keep everything" in {
+    val expectedSet = Set("a", "b", "c")
+    val result = Strain.keep[String](expectedSet, a => true)
+    result should be(expectedSet)
+  }
+
+  it should "keep first and last" in {
+    val result = Strain.keep[Int](Set(1, 2, 3), x => x % 2 != 0)
+    result should be(Set(1, 3))
+  }
+
+  it should "keep neither first or last" in {
+    val result = Strain.keep[Int](Set(1, 2, 3, 4, 5), x => x % 2 == 0)
+    result should be(Set(2, 4))
+  }
+
+  it should "keep strings" in {
+    val words = "apple zebra banana zombies cherimoya zelot".split(' ')
+    val result = Strain.keep[String](words, s => s.startsWith("z"))
+    result should be(Array("zebra", "zombies", "zelot"))
+  }
+
+  it should "keep sequences" in {
+    val sequences = Seq(Seq(1, 2, 3), Seq(5, 5, 5), Seq(5, 1, 2), Seq(2, 1, 2),
+      Seq(1, 5, 2), Seq(2, 2, 1), Seq(1, 2, 5))
+    val expected = Seq(Seq(5, 5, 5), Seq(5, 1, 2), Seq(1, 5, 2), Seq(1, 2, 5));
+    val result = Strain.keep[Seq[Int]](sequences, seq => seq.contains(5))
+    result should be(expected)
+  }
+
+  it should "handle empty discard" in {
+    val result = Strain.discard[Int](List(), x => x < 10)
+    result should be(List.empty)
+  }
+
+  it should "discard first and last" in {
+    val result = Strain.discard[Int](Set(1, 2, 3), x => x % 2 != 0)
+    result should be(Set(2))
+  }
+
+  it should "discard neither first or last" in {
+    val result = Strain.discard[Int](Set(1, 2, 3, 4, 5), x => x % 2 == 0)
+    result should be(Set(1, 3, 5))
+  }
+
+  it should "discard strings" in {
+    val words = "apple zebra banana zombies cherimoya zelot".split(' ')
+    val result = Strain.discard[String](words, s => s.startsWith("z"))
+    result should be(Array("apple", "banana", "cherimoya"))
+  }
+
+  it should "discard sequences" in {
+    val sequences = Seq(Seq(1, 2, 3), Seq(5, 5, 5), Seq(5, 1, 2), Seq(2, 1, 2),
+      Seq(1, 5, 2), Seq(2, 2, 1), Seq(1, 2, 5))
+    val expected = Seq(Seq(1, 2, 3), Seq(2, 1, 2), Seq(2, 2, 1));
+    val result = Strain.discard[Seq[Int]](sequences, seq => seq.contains(5))
+    result should be(expected)
+  }
+}
