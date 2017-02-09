909ed460159c857ab8712899c0312a55cd9aba2e
Ric Emery <ric.emery@gmail.com>
2017-02-08 22:35:57 -0700

    Update Strain example and tests to not use for comprehension.

--- exercises/strain/example.scala
+++ exercises/strain/example.scala
@@ -1,7 +1,22 @@
-object Strain {
-  def keep[A](traversable: Traversable[A], func: A => Boolean): Traversable[A]
-    = for (a <- traversable if func(a)) yield a
+import scala.annotation.tailrec
 
-  def discard[A](traversable: Traversable[A], func: A => Boolean): Traversable[A]
-    = for (a <- traversable if !func(a)) yield a
+object Strain {
+  def keep[A](seq: Seq[A], func: A => Boolean): Seq[A] =
+    recurse(seq, func, List()).reverse
+
+  def discard[A](seq: Seq[A], func: A => Boolean): Seq[A] = {
+    val notFunc: A => Boolean = a => !func(a)
+    recurse(seq, notFunc, List()).reverse
+  }
+
+  @tailrec
+  private def recurse[A](seq: Seq[A], func: A => Boolean, acc: Seq[A]): Seq[A] =
+    seq match {
+      case Nil => acc
+      case x::xs =>
+        if (func(x))
+          recurse(xs, func, x +: acc)
+        else
+          recurse(xs, func, acc)
+    }
 }
--- exercises/strain/src/test/scala/StrainTest.scala
+++ exercises/strain/src/test/scala/StrainTest.scala
@@ -8,70 +8,70 @@ class StrainTest extends FlatSpec with Matchers {
   }
 
   it should "keep everything" in {
-    pending
-    val expectedSet = Set("a", "b", "c")
-    val result = Strain.keep[String](expectedSet, a => true)
-    result should be(expectedSet)
+    //pending
+    val expectedList = List("a", "b", "c")
+    val result = Strain.keep[String](expectedList, _ => true)
+    result should be(expectedList)
   }
 
   it should "keep first and last" in {
-    pending
-    val result = Strain.keep[Int](Set(1, 2, 3), x => x % 2 != 0)
-    result should be(Set(1, 3))
+    //pending
+    val result = Strain.keep[Int](List(1, 2, 3), x => x % 2 != 0)
+    result should be(List(1, 3))
   }
 
   it should "keep neither first or last" in {
-    pending
-    val result = Strain.keep[Int](Set(1, 2, 3, 4, 5), x => x % 2 == 0)
-    result should be(Set(2, 4))
+    //pending
+    val result = Strain.keep[Int](List(1, 2, 3, 4, 5), x => x % 2 == 0)
+    result should be(List(2, 4))
   }
 
   it should "keep strings" in {
-    pending
-    val words = "apple zebra banana zombies cherimoya zelot".split(' ')
+    //pending
+    val words = List("apple",  "zebra", "banana", "zombies", "cherimoya",  "zelot")
     val result = Strain.keep[String](words, s => s.startsWith("z"))
-    result should be(Array("zebra", "zombies", "zelot"))
+    result should be(List("zebra", "zombies", "zelot"))
   }
 
   it should "keep sequences" in {
-    pending
+    //pending
     val sequences = Seq(Seq(1, 2, 3), Seq(5, 5, 5), Seq(5, 1, 2), Seq(2, 1, 2),
       Seq(1, 5, 2), Seq(2, 2, 1), Seq(1, 2, 5))
-    val expected = Seq(Seq(5, 5, 5), Seq(5, 1, 2), Seq(1, 5, 2), Seq(1, 2, 5));
+    val expected = Seq(Seq(5, 5, 5), Seq(5, 1, 2), Seq(1, 5, 2), Seq(1, 2, 5))
     val result = Strain.keep[Seq[Int]](sequences, seq => seq.contains(5))
     result should be(expected)
   }
 
   it should "handle empty discard" in {
-    pending
+    //pending
     val result = Strain.discard[Int](List(), x => x < 10)
     result should be(List.empty)
   }
 
   it should "discard first and last" in {
-    pending
-    val result = Strain.discard[Int](Set(1, 2, 3), x => x % 2 != 0)
-    result should be(Set(2))
+    //pending
+    val result = Strain.discard[Int](List(1, 2, 3), x => x % 2 != 0)
+    result should be(List(2))
   }
 
   it should "discard neither first or last" in {
-    pending
-    val result = Strain.discard[Int](Set(1, 2, 3, 4, 5), x => x % 2 == 0)
-    result should be(Set(1, 3, 5))
+    //pending
+    val result = Strain.discard[Int](List(1, 2, 3, 4, 5), x => x % 2 == 0)
+    result should be(List(1, 3, 5))
   }
 
   it should "discard strings" in {
-    pending
-    val words = "apple zebra banana zombies cherimoya zelot".split(' ')
+    //pending
+    val words = List("apple",  "zebra", "banana", "zombies", "cherimoya",  "zelot")
     val result = Strain.discard[String](words, s => s.startsWith("z"))
-    result should be(Array("apple", "banana", "cherimoya"))
+    result should be(List("apple", "banana", "cherimoya"))
   }
 
   it should "discard sequences" in {
-    pending
+    //pending
     val sequences = Seq(Seq(1, 2, 3), Seq(5, 5, 5), Seq(5, 1, 2), Seq(2, 1, 2),
       Seq(1, 5, 2), Seq(2, 2, 1), Seq(1, 2, 5))
-    val expected = Seq(Seq(1, 2, 3), Seq(2, 1, 2), Seq(2, 2, 1));
+    val expected = Seq(Seq(1, 2, 3), Seq(2, 1, 2), Seq(2, 2, 1))
     val result = Strain.discard[Seq[Int]](sequences, seq => seq.contains(5))
     result should be(expected)
   }
