30c13ebe8d7d9584702e747a23494cf3974937e4
Ric Emery <ric.emery@gmail.com>
2017-02-09 08:40:37 -0700

    Uncomment pending

--- exercises/strain/src/test/scala/StrainTest.scala
+++ exercises/strain/src/test/scala/StrainTest.scala
@@ -8,33 +8,33 @@ class StrainTest extends FlatSpec with Matchers {
   }
 
   it should "keep everything" in {
-    //pending
+    pending
     val expectedList = List("a", "b", "c")
     val result = Strain.keep[String](expectedList, _ => true)
     result should be(expectedList)
   }
 
   it should "keep first and last" in {
-    //pending
+    pending
     val result = Strain.keep[Int](List(1, 2, 3), x => x % 2 != 0)
     result should be(List(1, 3))
   }
 
   it should "keep neither first or last" in {
-    //pending
+    pending
     val result = Strain.keep[Int](List(1, 2, 3, 4, 5), x => x % 2 == 0)
     result should be(List(2, 4))
   }
 
   it should "keep strings" in {
-    //pending
+    pending
     val words = List("apple",  "zebra", "banana", "zombies", "cherimoya",  "zelot")
     val result = Strain.keep[String](words, s => s.startsWith("z"))
     result should be(List("zebra", "zombies", "zelot"))
   }
 
   it should "keep sequences" in {
-    //pending
+    pending
     val sequences = Seq(Seq(1, 2, 3), Seq(5, 5, 5), Seq(5, 1, 2), Seq(2, 1, 2),
       Seq(1, 5, 2), Seq(2, 2, 1), Seq(1, 2, 5))
     val expected = Seq(Seq(5, 5, 5), Seq(5, 1, 2), Seq(1, 5, 2), Seq(1, 2, 5))
@@ -43,32 +43,32 @@ class StrainTest extends FlatSpec with Matchers {
   }
 
   it should "handle empty discard" in {
-    //pending
+    pending
     val result = Strain.discard[Int](List(), x => x < 10)
     result should be(List.empty)
   }
 
   it should "discard first and last" in {
-    //pending
+    pending
     val result = Strain.discard[Int](List(1, 2, 3), x => x % 2 != 0)
     result should be(List(2))
   }
 
   it should "discard neither first or last" in {
-    //pending
+    pending
     val result = Strain.discard[Int](List(1, 2, 3, 4, 5), x => x % 2 == 0)
     result should be(List(1, 3, 5))
   }
 
   it should "discard strings" in {
-    //pending
+    pending
     val words = List("apple",  "zebra", "banana", "zombies", "cherimoya",  "zelot")
     val result = Strain.discard[String](words, s => s.startsWith("z"))
     result should be(List("apple", "banana", "cherimoya"))
   }
 
   it should "discard sequences" in {
-    //pending
+    pending
     val sequences = Seq(Seq(1, 2, 3), Seq(5, 5, 5), Seq(5, 1, 2), Seq(2, 1, 2),
       Seq(1, 5, 2), Seq(2, 2, 1), Seq(1, 2, 5))
     val expected = Seq(Seq(1, 2, 3), Seq(2, 1, 2), Seq(2, 2, 1))
