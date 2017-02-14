914647cb1e5f64c40f8f3dd959c7b6f38801cdbb
Ric Emery <ric.emery@gmail.com>
2017-02-13 20:19:44 -0700

    Update BinarySearchTest based on code review. Removed pending from first test case. Removed unnecessary [Int]. Made use of Optional more clear. Also changed to using TestBuilder within BinarySearchTestGenerator

--- exercises/binary-search/src/test/scala/BinarySearchTest.scala
+++ exercises/binary-search/src/test/scala/BinarySearchTest.scala
@@ -1,74 +1,63 @@
 import org.scalatest.{FunSuite, Matchers}
 
 class BinarySearchTest extends FunSuite with Matchers {
+
   test("finds a value in an array with one element") {
-    pending
-    val elements = Array[Int](6)
-    val result = BinarySearch.search(elements, 6)
-    assert(result.contains(0))
+    val result = BinarySearch.search(Array(6), 6)
+    result should be (Some(0))
   }
 
   test("finds a value in the middle of an array") {
     pending
-    val elements = Array[Int](1, 3, 4, 6, 8, 9, 11)
-    val result = BinarySearch.search(elements, 6)
-    assert(result.contains(3))
+    val result = BinarySearch.search(Array(1, 3, 4, 6, 8, 9, 11), 6)
+    result should be (Some(3))
   }
 
   test("finds a value at the beginning of an array") {
     pending
-    val elements = Array[Int](1, 3, 4, 6, 8, 9, 11)
-    val result = BinarySearch.search(elements, 1)
-    assert(result.contains(0))
+    val result = BinarySearch.search(Array(1, 3, 4, 6, 8, 9, 11), 1)
+    result should be (Some(0))
   }
 
   test("finds a value at the end of an array") {
     pending
-    val elements = Array[Int](1, 3, 4, 6, 8, 9, 11)
-    val result = BinarySearch.search(elements, 11)
-    assert(result.contains(6))
+    val result = BinarySearch.search(Array(1, 3, 4, 6, 8, 9, 11), 11)
+    result should be (Some(6))
   }
 
   test("finds a value in an array of odd length") {
     pending
-    val elements = Array[Int](1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634)
-    val result = BinarySearch.search(elements, 144)
-    assert(result.contains(9))
+    val result = BinarySearch.search(Array(1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634), 144)
+    result should be (Some(9))
   }
 
   test("finds a value in an array of even length") {
     pending
-    val elements = Array[Int](1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377)
-    val result = BinarySearch.search(elements, 21)
-    assert(result.contains(5))
+    val result = BinarySearch.search(Array(1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377), 21)
+    result should be (Some(5))
   }
 
   test("identifies that a value is not included in the array") {
     pending
-    val elements = Array[Int](1, 3, 4, 6, 8, 9, 11)
-    val result = BinarySearch.search(elements, 7)
-    assert(result.isEmpty)
+    val result = BinarySearch.search(Array(1, 3, 4, 6, 8, 9, 11), 7)
+    result should be (None)
   }
 
   test("a value smaller than the array's smallest value is not included") {
     pending
-    val elements = Array[Int](1, 3, 4, 6, 8, 9, 11)
-    val result = BinarySearch.search(elements, 0)
-    assert(result.isEmpty)
+    val result = BinarySearch.search(Array(1, 3, 4, 6, 8, 9, 11), 0)
+    result should be (None)
   }
 
   test("a value larger than the array's largest value is not included") {
     pending
-    val elements = Array[Int](1, 3, 4, 6, 8, 9, 11)
-    val result = BinarySearch.search(elements, 13)
-    assert(result.isEmpty)
+    val result = BinarySearch.search(Array(1, 3, 4, 6, 8, 9, 11), 13)
+    result should be (None)
   }
 
   test("nothing is included in an empty array") {
     pending
-    val elements = Array[Int]()
-    val result = BinarySearch.search(elements, 1)
-    assert(result.isEmpty)
+    val result = BinarySearch.search(Array[Int](), 1)
+    result should be (None)
   }
-
 }
