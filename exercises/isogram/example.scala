27a3bdb3a30ba7b6f58803ae23c9fcc509308265
Ric Emery <ric.emery@gmail.com>
2017-02-21 06:57:34 -0700

    Remove mutable hashmap from example

--- exercises/isogram/example.scala
+++ exercises/isogram/example.scala
@@ -1,19 +1,8 @@
-import scala.collection.mutable
-
 object Isogram {
   def isIsogram(s: String): Boolean = {
-    val seen = mutable.HashSet[Char]()
-
-    s.filter(_.isLetter)
+    !s.filter(_.isLetter)
       .map(_.toLower)
-      .foreach { c =>
-        if (seen.contains(c)) {
-          return false
-        } else {
-          seen.add(c)
-        }
-      }
-
-    true
+      .groupBy(c => c)
+      .values.exists(xs => xs.length > 1)
   }
 }
