fcced6de5e6346ca79e068888f72f4dedcb51d07
Ric Emery <ric.emery@gmail.com>
2017-02-22 08:44:55 -0700

    simplify example

--- exercises/isogram/example.scala
+++ exercises/isogram/example.scala
@@ -1,8 +1,8 @@
 object Isogram {
-  def isIsogram(s: String): Boolean = {
+  def isIsogram(s: String): Boolean =
     !s.filter(_.isLetter)
       .map(_.toLower)
-      .groupBy(c => c)
-      .values.exists(xs => xs.length > 1)
-  }
+      .groupBy(identity)
+      .values
+      .exists(_.length > 1)
 }
