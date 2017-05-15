c4971649b10fdd101f12cbab234b69ccc2143e77
Ric Emery <ric.emery@gmail.com>
2017-05-14 20:48:22 -0700

    Remove Option from required method signature. And, remove invalid Codon test

--- exercises/protein-translation/example.scala
+++ exercises/protein-translation/example.scala
@@ -1,14 +1,4 @@
-/**
- * Copyright 2017 © Denmar Technical Services Inc
- * The U.S. Government has unlimited rights per DFAR 252.227-7014, all other
- * rights reserved.
- * <p>
- * WARNING - This software contains Technical Data whose export is restricted by
- * the Arms Export Control Act (Title 22, U.S.C., Sec 2751, et seq.) or the
- * Export Administration Act of 1979, as amended (Title 50, U.S. C. App. 2401
- * et seq.). Violations of these export laws are subject to severe criminal
- * penalties.
- */
+import scala.annotation.tailrec
 
 object ProteinTranslation {
 
@@ -31,15 +21,18 @@ object ProteinTranslation {
     "UAG" -> "STOP",
     "UGA" -> "STOP")
 
-  def translate(input: String): Option[Seq[String]] = {
+  /**
+    * Note that this solution ignores invalid Codons.
+    */
+  def translate(input: String): Seq[String] = {
     @tailrec
-    def translate_internal(acc: Seq[String], strs: Seq[String]): Option[Seq[String]] = {
+    def translate_internal(acc: Seq[String], strs: Seq[String]): Seq[String] = {
       strs match {
         case x::xs => translations get x match {
-          case Some(codon) => if ("STOP".equals(codon)) Some(acc.reverse) else translate_internal(codon +: acc, xs)
-          case _ => None
+          case Some(codon) => if ("STOP".equals(codon)) acc.reverse else translate_internal(codon +: acc, xs)
+          case _ => translate_internal(acc, xs)
         }
-        case _ => Some(acc.reverse)
+        case _ => acc.reverse
       }
     }
 
--- exercises/protein-translation/src/test/scala/ProteinTranslationTest.scala
+++ exercises/protein-translation/src/test/scala/ProteinTranslationTest.scala
@@ -2,64 +2,59 @@ import org.scalatest.{FunSuite, Matchers}
 
 class ProteinTranslationTest extends FunSuite with Matchers {
   test("Identifies methionine codon") {
-    ProteinTranslation.translate("AUG") should be(Some(Seq("Methionine")))
+    ProteinTranslation.translate("AUG") should be(Seq("Methionine"))
   }
 
   test("Identifies phenylalanine codons") {
     pending
-    ProteinTranslation.translate("UUU") should be(Some(Seq("Phenylalanine")))
-    ProteinTranslation.translate("UUC") should be(Some(Seq("Phenylalanine")))
+    ProteinTranslation.translate("UUU") should be(Seq("Phenylalanine"))
+    ProteinTranslation.translate("UUC") should be(Seq("Phenylalanine"))
   }
 
   test("Identifies leucine codons") {
     pending
-    ProteinTranslation.translate("UUA") should be(Some(Seq("Leucine")))
-    ProteinTranslation.translate("UUG") should be(Some(Seq("Leucine")))
+    ProteinTranslation.translate("UUA") should be(Seq("Leucine"))
+    ProteinTranslation.translate("UUG") should be(Seq("Leucine"))
   }
 
   test("Identifies serine codons") {
     pending
-    ProteinTranslation.translate("UCU") should be(Some(Seq("Serine")))
-    ProteinTranslation.translate("UCC") should be(Some(Seq("Serine")))
-    ProteinTranslation.translate("UCA") should be(Some(Seq("Serine")))
-    ProteinTranslation.translate("UCG") should be(Some(Seq("Serine")))
+    ProteinTranslation.translate("UCU") should be(Seq("Serine"))
+    ProteinTranslation.translate("UCC") should be(Seq("Serine"))
+    ProteinTranslation.translate("UCA") should be(Seq("Serine"))
+    ProteinTranslation.translate("UCG") should be(Seq("Serine"))
   }
 
   test("Identifies tyrosine codons") {
     pending
-    ProteinTranslation.translate("UAU") should be(Some(Seq("Tyrosine")))
-    ProteinTranslation.translate("UAC") should be(Some(Seq("Tyrosine")))
+    ProteinTranslation.translate("UAU") should be(Seq("Tyrosine"))
+    ProteinTranslation.translate("UAC") should be(Seq("Tyrosine"))
   }
 
   test("Identifies cysteine codons") {
     pending
-    ProteinTranslation.translate("UGU") should be(Some(Seq("Cysteine")))
-    ProteinTranslation.translate("UGU") should be(Some(Seq("Cysteine")))
+    ProteinTranslation.translate("UGU") should be(Seq("Cysteine"))
+    ProteinTranslation.translate("UGU") should be(Seq("Cysteine"))
   }
 
   test("Identifies tryptophan codons") {
     pending
-    ProteinTranslation.translate("UGG") should be(Some(Seq("Tryptophan")))
-    ProteinTranslation.translate("UGG") should be(Some(Seq("Tryptophan")))
+    ProteinTranslation.translate("UGG") should be(Seq("Tryptophan"))
+    ProteinTranslation.translate("UGG") should be(Seq("Tryptophan"))
   }
 
   test("Translate RNA strand into correct protein") {
     pending
-    ProteinTranslation.translate("AUGUUUUGG") should be(Some(Seq("Methionine", "Phenylalanine", "Tryptophan")))
+    ProteinTranslation.translate("AUGUUUUGG") should be(Seq("Methionine", "Phenylalanine", "Tryptophan"))
   }
 
   test("Stops translation if stop codon is present") {
     pending
-    ProteinTranslation.translate("AUGUUUUAA") should be(Some(Seq("Methionine", "Phenylalanine")))
+    ProteinTranslation.translate("AUGUUUUAA") should be(Seq("Methionine", "Phenylalanine"))
   }
 
   test("Stops translation of longest strand") {
     pending
-    ProteinTranslation.translate("UGGUGUUAUUAAUGGUUU") should be(Some(Seq("Tryptophan", "Cysteine", "Tyrosine")))
-  }
-
-  test("Return NONE for invalid Codon") {
-    pending
-    ProteinTranslation.translate("CARROT") should be(None)
+    ProteinTranslation.translate("UGGUGUUAUUAAUGGUUU") should be(Seq("Tryptophan", "Cysteine", "Tyrosine"))
   }
 }
