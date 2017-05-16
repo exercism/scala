13d2a8e110131e735338af4c8bffebadac7ddd2f
Ric Emery <ric.emery@gmail.com>
2017-05-15 20:46:16 -0700

    Add file with expected interface. Refs #137

---
+++ exercises/grade-school/src/main/scala/School.scala
@@ -0,0 +1,11 @@
+class School {
+  type DB = Map[Int, Seq[String]]
+
+  def add(name: String, g: Int) = ???
+
+  def db: DB = ???
+
+  def grade(g: Int): Seq[String] = ???
+
+  def sorted: DB = ???
+}
