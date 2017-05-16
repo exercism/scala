1ede59cc324c9ace15b030d889e6219115dea5c5
Ric Emery <ric.emery@gmail.com>
2017-05-15 20:32:06 -0700

    Add file with expected interface. Refs #137

---
+++ exercises/gigasecond/src/main/scala/Gigasecond.scala
@@ -0,0 +1,8 @@
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+object Gigasecond {
+  def addGigaseconds(startDate: LocalDate): LocalDateTime = ???
+
+  def addGigaseconds(startDateTime: LocalDateTime): LocalDateTime = ???
+}
