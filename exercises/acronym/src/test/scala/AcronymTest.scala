5dabeeb5e388a9ecbfcea003130092ff9b0b8d99
abo64 <abo64@gmx.de>
2017-06-12 22:29:31 +0200

    Add test suite generator and version to acronym (#370)

--- exercises/acronym/src/test/scala/AcronymTest.scala
+++ exercises/acronym/src/test/scala/AcronymTest.scala
@@ -1,15 +1,35 @@
-import org.scalatest.{FlatSpec, Matchers}
 
-class AcronymTest extends FlatSpec with Matchers {
-  private val acronyms = List(
-    ("Portable Network Graphics", "PNG"),
-    ("Ruby on Rails", "ROR"),
-    ("HyperText Markup Language", "HTML"),
-    ("First In, First Out",  "FIFO"),
-    ("PHP: Hypertext Preprocessor", "PHP"),
-    ("Complementary metal-oxide semiconductor", "CMOS"))
+import org.scalatest.{Matchers, FunSuite}
 
-  it should "create acronyms" in {
-    acronyms.foreach{case (phrase, acronym) => Acronym.abbreviate(phrase) should equal(acronym)}
+/** @version 1.1.0 */
+class AcronymTest extends FunSuite with Matchers {
+
+  test("basic") { 
+    Acronym.abbreviate("Portable Network Graphics") should be ("PNG")
+  }
+
+  test("lowercase words") { 
+    pending
+    Acronym.abbreviate("Ruby on Rails") should be ("ROR")
+  }
+
+  test("punctuation") { 
+    pending
+    Acronym.abbreviate("First In, First Out") should be ("FIFO")
+  }
+
+  test("all caps words") { 
+    pending
+    Acronym.abbreviate("PHP: Hypertext Preprocessor") should be ("PHP")
+  }
+
+  test("non-acronym all caps word") { 
+    pending
+    Acronym.abbreviate("GNU Image Manipulation Program") should be ("GIMP")
+  }
+
+  test("hyphenated") { 
+    pending
+    Acronym.abbreviate("Complementary metal-oxide semiconductor") should be ("CMOS")
   }
 }
