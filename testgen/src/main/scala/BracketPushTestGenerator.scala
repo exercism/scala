41e149a67248e2fc592ae6b9996978b42c96beee
Ric Emery <ric.emery@gmail.com>
2017-07-31 21:38:36 -0700

    Update BracketPushTestGenerator. Update BracketPushTest. Refs #370, #331

--- testgen/src/main/scala/BracketPushTestGenerator.scala
+++ testgen/src/main/scala/BracketPushTestGenerator.scala
@@ -1,44 +1,15 @@
-import play.api.libs.json.Json
+import java.io.File
 
-import scala.io.Source
-
-class BracketPushTestGenerator {
-  implicit val testCaseReader = Json.reads[BracketTestCase]
-
-  private val filename = "bracket-push.json"
-  private val fileContents = Source.fromFile(filename).getLines.mkString
-  private val json = Json.parse(fileContents)
-
-  def write {
-    print("import org.scalatest.{FunSuite, Matchers}" + System.lineSeparator())
-    print(System.lineSeparator())
-    print("class BracketsTest extends FunSuite with Matchers {" + System.lineSeparator())
-
-    writeTestCases()
-
-    print("}" + System.lineSeparator())
-  }
-
-  private def writeTestCases(): Unit = {
-    val testCases = (json \ "cases").get.as[List[BracketTestCase]]
-
-    testCases.foreach(tc => {
-      print("\ttest(\"" + tc.description + "\") {" + System.lineSeparator())
-      println("pending")
-      println("Brackets.areBalanced(\"\"\"" + tc.input + "\"\"\") should be (" + tc.expected + ")")
-
-      print("\t}" + System.lineSeparator())
-      print(System.lineSeparator())
-    })
-  }
-}
-
-case class BracketTestCase(description: String,
-                           input: String,
-                           expected: Boolean)
+import testgen.TestSuiteBuilder._
+import testgen._
 
 object BracketPushTestGenerator {
   def main(args: Array[String]): Unit = {
-    new BracketPushTestGenerator().write
+    val file = new File("src/main/resources/bracket-push.json")
+
+    val code = TestSuiteBuilder.build(file, fromLabeledTest("input"))
+    println(s"-------------")
+    println(code)
+    println(s"-------------")
   }
 }
