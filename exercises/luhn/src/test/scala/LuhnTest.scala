61a0fcc28c3269ffc4c16d6a17bb1a03bf894916
Ric Emery <ric.emery@gmail.com>
2017-08-07 21:03:45 -0700

    Update LuhnTestGenerator. Update LuhnTest. Refs #370, #331

--- exercises/luhn/src/test/scala/LuhnTest.scala
+++ exercises/luhn/src/test/scala/LuhnTest.scala
@@ -1,33 +1,69 @@
-import org.scalatest.{FunSuite, Matchers}
+import org.scalatest.{Matchers, FunSuite}
 
+/** @version 1.0.0 */
 class LuhnTest extends FunSuite with Matchers {
+
   test("single digit strings can not be valid") {
-    Luhn.validate("1") should be (false)
+    Luhn.valid("1") should be (false)
   }
 
   test("A single zero is invalid") {
     pending
-    Luhn.validate("0") should be (false)
+    Luhn.valid("0") should be (false)
   }
 
-  test("valid Canadian SIN") {
+  test("a simple valid SIN that remains valid if reversed") {
     pending
-    Luhn.validate("046 454 286") should be (true)
+    Luhn.valid("059") should be (true)
+  }
+
+  test("a simple valid SIN that becomes invalid if reversed") {
+    pending
+    Luhn.valid("59") should be (true)
+  }
+
+  test("a valid Canadian SIN") {
+    pending
+    Luhn.valid("055 444 285") should be (true)
   }
 
   test("invalid Canadian SIN") {
     pending
-    Luhn.validate("046 454 287") should be (false)
+    Luhn.valid("055 444 286") should be (false)
   }
 
   test("invalid credit card") {
     pending
-    Luhn.validate("8273 1232 7352 0569") should be (false)
+    Luhn.valid("8273 1232 7352 0569") should be (false)
   }
 
-  test("strings that contain non-digits are not valid") {
+  test("valid strings with a non-digit included become invalid") {
     pending
-    Luhn.validate("827a 1232 7352 0569") should be (false)
+    Luhn.valid("055a 444 285") should be (false)
+  }
+
+  test("valid strings with punctuation included become invalid") {
+    pending
+    Luhn.valid("055-444-285") should be (false)
+  }
+
+  test("valid strings with symbols included become invalid") {
+    pending
+    Luhn.valid("055£ 444$ 285") should be (false)
+  }
+
+  test("single zero with space is invalid") {
+    pending
+    Luhn.valid(" 0") should be (false)
+  }
+
+  test("more than a single zero is valid") {
+    pending
+    Luhn.valid("0000 0") should be (true)
+  }
+
+  test("input digit 9 is correctly converted to output digit 9") {
+    pending
+    Luhn.valid("091") should be (true)
   }
 }
-
