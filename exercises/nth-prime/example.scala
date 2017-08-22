ef818ad4d09ee5e00b5c95ed32be0ac662770821
Ric Emery <ric.emery@gmail.com>
2017-08-21 20:34:11 -0700

    Create NthPrimeTestGenerator. Update NthPrimeChainTest. Refs #370, #331

--- exercises/nth-prime/example.scala
+++ exercises/nth-prime/example.scala
@@ -1,10 +1,10 @@
-object Prime {
+object NthPrime {
   // The stream will cache calculated primes. This is nice for performance,
   // but not so nice for memory utilization.
   private lazy val primes: Stream[BigInt] =
     Stream.cons(BigInt(2), primes.map(b => new BigInt(b.bigInteger.nextProbablePrime)))
 
-  def nth(n: Int): Option[Int] =
+  def prime(n: Int): Option[Int] =
     if (n < 1) None
     else Some(primes.drop(n - 1).head.toInt)
 }
