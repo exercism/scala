f0386862f9630147a82c94815f53837c9202deb3
Ric Emery <ric.emery@gmail.com>
2017-09-13 13:09:28 -0700

    Add version info to ZebraPuzzleTest.scala. Refs #370, #331

--- exercises/zebra-puzzle/src/test/scala/ZebraPuzzleTest.scala
+++ exercises/zebra-puzzle/src/test/scala/ZebraPuzzleTest.scala
@@ -2,6 +2,7 @@ import org.scalatest.{FunSuite, Matchers}
 
 import ZebraPuzzle._
 
+/** @version created manually **/
 class ZebraPuzzleTest extends FunSuite with Matchers {
   test("solve the Zebra Puzzle") {
     ZebraPuzzle.solve should be (
