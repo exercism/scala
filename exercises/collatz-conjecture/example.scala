0121f59cd316268224f08076f6c9eed7f8f04ee8
Ric Emery <ric.emery@gmail.com>
2017-10-09 20:31:00 -0700

    Add error tests. Change signature to use Option

--- exercises/collatz-conjecture/example.scala
+++ exercises/collatz-conjecture/example.scala
@@ -1,15 +1,17 @@
 object CollatzConjecture {
 
-  def isEven( v:Int ) : Boolean = v % 2 == 0
+  def isEven( v:Int ): Boolean = v % 2 == 0
 
-  def collatz_h(n: Int, acc: Int) : Int ={
-    if (n == 1){
-      acc
-    }else if (isEven(n)){
+  def collatz_h(n: Int, acc: Int): Option[Int] = {
+    if (n <= 0)
+      None
+    else if (n == 1){
+      Some(acc)
+    } else if (isEven(n)){
       collatz_h(n/2, acc+1)
-    }else{
+    } else{
       collatz_h(n*3 + 1, acc+1)}
   }
 
-  def collatz(n: Int) : Int = collatz_h(n, 0)
+  def collatz(n: Int): Option[Int] = collatz_h(n, 0)
 }
--- exercises/collatz-conjecture/src/test/scala/CollatzConjectureTest.scala
+++ exercises/collatz-conjecture/src/test/scala/CollatzConjectureTest.scala
@@ -3,28 +3,31 @@ import org.scalatest.{Matchers, FunSuite}
 class CollatzConjectureTest extends FunSuite with Matchers {
 
   test("zero steps for one") {
-    pending
-    CollatzConjecture.collatz(1) should be (0)
+    CollatzConjecture.collatz(1) should be (Some(0))
   }
 
   test("divide if even") {
     pending
-    CollatzConjecture.collatz(16) should be (4)
+    CollatzConjecture.collatz(16) should be (Some(4))
   }
 
   test("even and odd steps") {
     pending
-    CollatzConjecture.collatz(12) should be (9)
+    CollatzConjecture.collatz(12) should be (Some(9))
   }
 
   test("Large number of even and odd steps") {
     pending
-    CollatzConjecture.collatz(1000000) should be (152)
+    CollatzConjecture.collatz(1000000) should be (Some(152))
   }
 
-  test("Large number of even and odd steps") {
+  test("zero is an error") {
     pending
-    CollatzConjecture.collatz(1000000) should be (152)
+    CollatzConjecture.collatz(0) should be (None)
   }
 
+  test("negative value is an error") {
+    pending
+    CollatzConjecture.collatz(-15) should be (None)
+  }
 }
