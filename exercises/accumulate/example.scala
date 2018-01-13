8e3aa190fd6d04e843cdce0779df93285eaa68e4
Carlos R Vargas Montero <carlos@zasumarket.com>
2018-01-13 02:24:03 -0600

    Accumulate solution

--- exercises/accumulate/example.scala
+++
@@ -1,7 +0,0 @@
-class Accumulate {
-  def accumulate[A, B](f: (A) => B, list : List[A]): List[B] =
-    list match {
-      case Nil => Nil
-      case x::xs => f(x) :: accumulate(f, xs)
-    }
-}
--- exercises/accumulate/src/main/scala/Accumulate.scala
+++ exercises/accumulate/src/main/scala/Accumulate.scala
@@ -1,3 +1,17 @@
+import scala.annotation.tailrec
+
 class Accumulate {
-  def accumulate[A, B](f: (A) => B, list : List[A]): List[B] = ???
+  def accumulate[A, B](f: (A) => B, list : List[A]): List[B] = list match {
+    case Nil => Nil
+    case h :: t => f(h) :: accumulate(f, t)
+  }
+
+  def accumulateTailRecursive[A, B](f: (A) => B, list : List[A]): List[B] = {
+    @tailrec
+    def go(result: List[B], remaining: List[A]): List[B] = remaining match {
+      case Nil => result.reverse
+      case h :: t => go(f(h) :: result, t)
+    }
+    go(Nil, list)
+  }
 }
