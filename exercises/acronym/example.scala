633c0c22c896ce10eee6932ce62ba4ccd6fb94c0
Carlos R Vargas Montero <carlos@zasumarket.com>
2018-01-13 02:25:12 -0600

    Acronym solution

--- exercises/acronym/example.scala
+++ exercises/acronym/example.scala
@@ -1,5 +1,5 @@
-object Acronym {
+/*object Acronym {
   def abbreviate(phrase: String): String = {
     "[A-Z]+[a-z]*|[a-z]+".r.findAllIn(phrase).map(s => s.head.toUpper).mkString
   }
-}
+}*/
--- exercises/acronym/src/main/scala/Acronym.scala
+++ exercises/acronym/src/main/scala/Acronym.scala
@@ -1,3 +1,19 @@
 object Acronym {
-  def abbreviate(phrase: String): String = ???
+
+  sealed trait Action
+  case object Capture extends Action
+  case object Scan extends Action
+
+  case class State(tla: String, action: Action)
+
+  def abbreviate(phrase: String): String =
+    phrase.foldLeft(State("", Capture))((state, char) =>
+      state match {
+      case State(tla, Scan) =>
+        if(char == ' ') State(tla, Capture)
+        else State(tla, Scan)
+      case State(tla, Capture) =>
+        if(char == ' ') State(tla, Capture)
+        else State(tla + char, Scan)
+    }).tla
 }
