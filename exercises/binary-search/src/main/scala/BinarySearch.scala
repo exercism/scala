529f83220f7e03c5f267384bff25ca161f32b43e
Carlos R Vargas Montero <carlos@zasumarket.com>
2018-01-13 21:31:24 -0600

    Running all tests and fixing: - binary-search

--- exercises/binary-search/src/main/scala/BinarySearch.scala
+++ exercises/binary-search/src/main/scala/BinarySearch.scala
@@ -1,12 +1,19 @@
+import scala.annotation.tailrec
+
 object BinarySearch {
   def find(list: List[Int], element: Int): Option[Int] = {
+    @tailrec
     def go(array: Array[Int], element: Int, lowerBound: Int, upper: Int): Option[Int] = {
       val midpoint = lowerBound + ((upper - lowerBound) / 2)
       if(element == array(midpoint)) Some(midpoint)
-      else if(element < array(midpoint)) go(array, element, lowerBound, midpoint - 1)
-      else go(array, element, midpoint + 1, upper)
+      else if(element < array(midpoint) && midpoint > lowerBound) go(array, element, lowerBound, midpoint - 1)
+      else if(element >= array(midpoint) && midpoint < upper) go(array, element, midpoint + 1, upper)
+      else None
     }
-    val array = list.toArray
-    go(array, element, 0, array.length)
+    if(list.isEmpty) None
+    else {
+      val array = list.toArray
+      go(array, element, 0, array.length - 1)
+    }
   }
 }
--- exercises/binary-search/src/test/scala/BinarySearchTest.scala
+++ exercises/binary-search/src/test/scala/BinarySearchTest.scala
@@ -8,47 +8,38 @@ class BinarySearchTest extends FunSuite with Matchers {
   }
 
   test("finds a value in the middle of an array") {
-    pending
     BinarySearch.find(List(1, 3, 4, 6, 8, 9, 11), 6) should be (Some(3))
   }
 
   test("finds a value at the beginning of an array") {
-    pending
     BinarySearch.find(List(1, 3, 4, 6, 8, 9, 11), 1) should be (Some(0))
   }
 
   test("finds a value at the end of an array") {
-    pending
     BinarySearch.find(List(1, 3, 4, 6, 8, 9, 11), 11) should be (Some(6))
   }
 
   test("finds a value in an array of odd length") {
-    pending
     BinarySearch.find(List(1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634), 144) should be (Some(9))
   }
 
   test("finds a value in an array of even length") {
-    pending
     BinarySearch.find(List(1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377), 21) should be (Some(5))
   }
 
   test("identifies that a value is not included in the array") {
-    pending
     BinarySearch.find(List(1, 3, 4, 6, 8, 9, 11), 7) should be (None)
   }
 
   test("a value smaller than the array's smallest value is not included") {
-    pending
     BinarySearch.find(List(1, 3, 4, 6, 8, 9, 11), 0) should be (None)
   }
 
   test("a value larger than the array's largest value is not included") {
-    pending
     BinarySearch.find(List(1, 3, 4, 6, 8, 9, 11), 13) should be (None)
   }
 
   test("nothing is included in an empty array") {
-    pending
     BinarySearch.find(List(), 1) should be (None)
   }
 }
