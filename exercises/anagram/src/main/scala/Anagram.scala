2f5f8318694e7efd56561728097e0253ad87e221
Carlos R Vargas Montero <carlos@zasumarket.com>
2018-01-13 18:20:41 -0600

    Running all tests and fixing: - anagram

--- exercises/anagram/src/main/scala/Anagram.scala
+++ exercises/anagram/src/main/scala/Anagram.scala
@@ -1,7 +1,12 @@
 object Anagram {
-  def anagrams(word: String, anagrams: Seq[String]): List[String] = {
-    val lookupWordMap = wordToMap(word)
-    anagrams.filter(testWord => wordToMap(testWord) == lookupWordMap).toList
+  def anagrams(word: String, anagram: Seq[String]): List[String] = {
+    val wordLower = word.toLowerCase()
+    val lookupWordMap = wordToMap(wordLower)
+    anagram.filter(testWord => {
+      val testWordLower = testWord.toLowerCase()
+      testWordLower.toLowerCase != wordLower &&
+        wordToMap(testWordLower) == lookupWordMap
+    }).toList
   }
   import scala.collection.mutable
   def wordToMap(word: String): Map[Char, Int] =
--- exercises/anagram/src/test/scala/AnagramTest.scala
+++ exercises/anagram/src/test/scala/AnagramTest.scala
@@ -9,34 +9,28 @@ class AnagramTest extends FunSuite with Matchers {
   }
 
   test("detects simple anagram") {
-    pending
     Anagram.anagrams("ant", List("tan", "stand", "at")) should be(List("tan"))
   }
 
   test("does not detect false positives") {
-    pending
     Anagram.anagrams("galea", List("eagle")) should be(List())
   }
 
   test("detects two anagrams") {
-    pending
     Anagram.anagrams("master", List("stream", "pigeon", "maters")) should be(
       List("stream", "maters"))
   }
 
   test("does not detect anagram subsets") {
-    pending
     Anagram.anagrams("good", List("dog", "goody")) should be(List())
   }
 
   test("detects anagram") {
-    pending
     Anagram.anagrams("listen", List("enlists", "google", "inlets", "banana")) should be(
       List("inlets"))
   }
 
   test("detects three anagrams") {
-    pending
     Anagram.anagrams("allergy",
                      List("gallery",
                           "ballerina",
@@ -48,7 +42,6 @@ class AnagramTest extends FunSuite with Matchers {
   }
 
   test("does not detect identical words") {
-    pending
     Anagram.anagrams(
       "corn",
       List("corn", "dark", "Corn", "rank", "CORN", "cron", "park")) should be(
@@ -56,48 +49,40 @@ class AnagramTest extends FunSuite with Matchers {
   }
 
   test("does not detect non-anagrams with identical checksum") {
-    pending
     Anagram.anagrams("mass", List("last")) should be(List())
   }
 
   test("detects anagrams case-insensitively") {
-    pending
     Anagram.anagrams("Orchestra",
                      List("cashregister", "Carthorse", "radishes")) should be(
       List("Carthorse"))
   }
 
   test("detects anagrams using case-insensitive subject") {
-    pending
     Anagram.anagrams("Orchestra",
                      List("cashregister", "carthorse", "radishes")) should be(
       List("carthorse"))
   }
 
   test("detects anagrams using case-insensitive possible matches") {
-    pending
     Anagram.anagrams("orchestra",
                      List("cashregister", "Carthorse", "radishes")) should be(
       List("Carthorse"))
   }
 
   test("does not detect a word as its own anagram") {
-    pending
     Anagram.anagrams("banana", List("Banana")) should be(List())
   }
 
   test("does not detect a anagram if the original word is repeated") {
-    pending
     Anagram.anagrams("go", List("go Go GO")) should be(List())
   }
 
   test("anagrams must use all letters exactly once") {
-    pending
     Anagram.anagrams("tapper", List("patter")) should be(List())
   }
 
   test("capital word is not own anagram") {
-    pending
     Anagram.anagrams("BANANA", List("Banana")) should be(List())
   }
 }
