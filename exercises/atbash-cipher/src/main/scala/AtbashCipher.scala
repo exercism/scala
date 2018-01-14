6c44fa4c00eab6f445f64f02a416eb32c6f31ff4
Carlos R Vargas Montero <carlos@zasumarket.com>
2018-01-13 20:45:38 -0600

    Running all tests and fixing: - atbash-cipher

--- exercises/atbash-cipher/src/main/scala/AtbashCipher.scala
+++ exercises/atbash-cipher/src/main/scala/AtbashCipher.scala
@@ -1,12 +1,26 @@
 object AtbashCipher {
 
-  private val alphabet = 'a' to 'z'
+  private val alphabet = ('a' to 'z')
   private val reverseAlphabet = alphabet.reverse
   private val encodeMap: Map[Char, Char] = alphabet.zip(reverseAlphabet).toMap
   private val decodeMap: Map[Char, Char] = reverseAlphabet.zip(alphabet).toMap
 
-  def encode(input: String): String =
-    input.map(char => encodeMap.get(char)).flatten.mkString
-  def decode(input: String): String =
-    input.map(char => decodeMap.get(char)).flatten.mkString
+  def encode(input: String): String = {
+    encodeDecode(input, encodeMap)
+      .foldLeft((0,""))((accum, char) => {
+        if(accum._1 == 5)
+          (1, accum._2 + " " + char)
+        else
+          (accum._1 + 1, accum._2 + char)
+      })._2
+  }
+  def decode(input: String): String = {
+    encodeDecode(input, decodeMap).mkString
+  }
+
+  def encodeDecode(input: String, charMap: Map[Char, Char]): Seq[Char] = {
+    input.toLowerCase
+      .map(char => if(char.isDigit) Some(char) else charMap.get(char))
+      .flatten
+  }
 }
