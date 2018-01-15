257f927d2213d441efb5d4a9b29f0d9ee0a934b4
Carlos R Vargas Montero <cvargas.cr@gmail.com>
2018-01-15 02:56:44 -0600

    Eliminating uneeded print

--- exercises/collatz-conjecture/src/main/scala/CollatzConjecture.scala
+++ exercises/collatz-conjecture/src/main/scala/CollatzConjecture.scala
@@ -4,7 +4,6 @@ object CollatzConjecture {
   def steps(n: Int): Option[Int] = {
     @tailrec
     def go(n: Int, accum: Int): Int = {
-      println(s"go($n,$accum)")
       if (n == 1) accum
       else if (n % 2 == 0) go(n / 2, accum + 1)
       else go((3 * n) + 1, accum + 1)
