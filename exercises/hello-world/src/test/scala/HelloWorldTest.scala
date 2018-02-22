70aa3ce447c07c0345338ed8a4ce230b59c2c25b
Ric Emery <ric.emery@gmail.com>
2018-02-22 10:28:05 -0700

    Update hello-world exercise to match latest canonical json. Refs #488

--- exercises/hello-world/src/test/scala/HelloWorldTest.scala
+++ exercises/hello-world/src/test/scala/HelloWorldTest.scala
@@ -1,10 +1,9 @@
 import org.scalatest.{Matchers, FunSuite}
 
-/** @version 1.0.0 */
+/** @version 1.1.0 */
 class HelloWorldTest extends FunSuite with Matchers {
 
-  test("Say Hi!") { 
+  test("Say Hi!") {
     HelloWorld.hello() should be ("Hello, World!")
   }
 }
-
