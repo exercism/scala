92836fa81c952499b6ae6a089a8a70bb608e7737
Ric Emery <ric.emery@gmail.com>
2018-02-22 12:59:25 -0700

    Update matrix exercise to match latest canonical json. Refs #488

--- exercises/matrix/example.scala
+++ exercises/matrix/example.scala
@@ -1,5 +1,9 @@
 case class Matrix(matrix: String) {
   lazy val rows: Vector[Vector[Int]] = matrix.lines.map(_.split(' ').map(_.toInt).toVector).toVector
 
-  lazy val cols: Vector[Vector[Int]] = rows.transpose
+  lazy val columns: Vector[Vector[Int]] = rows.transpose
+
+  def row(i: Int) = rows(i)
+
+  def column(i: Int) = columns(i)
 }
