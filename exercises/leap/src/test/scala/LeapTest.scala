fbf8fd1aff9381876d856a1081b3c86b0263ddff
Ric Emery <ric.emery@gmail.com>
2018-02-22 11:14:29 -0700

    Update leapt exercise to match latest canonical json. Refs #488

--- exercises/leap/src/test/scala/LeapTest.scala
+++ exercises/leap/src/test/scala/LeapTest.scala
@@ -1,6 +1,6 @@
 import org.scalatest.{Matchers, FunSuite}
 
-/** @version 1.0.0 */
+/** @version 1.3.0 */
 class LeapTest extends FunSuite with Matchers {
 
   test("year not divisible by 4: common year") {
@@ -9,7 +9,7 @@ class LeapTest extends FunSuite with Matchers {
 
   test("year divisible by 4, not divisible by 100: leap year") {
     pending
-    Leap.leapYear(2016) should be (true)
+    Leap.leapYear(1996) should be (true)
   }
 
   test("year divisible by 100, not divisible by 400: common year") {
