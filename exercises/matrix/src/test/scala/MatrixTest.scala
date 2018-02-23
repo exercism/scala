0f3a2ca051d2134cbf301055b4db032912ac4171
Ric Emery <ric.emery@gmail.com>
2018-02-23 09:35:08 -0700

    Update matrix exercise to match latest canonical json. (#520)
    * Update matrix exercise to match latest canonical json.
    Refs #488

--- exercises/matrix/src/test/scala/MatrixTest.scala
+++ exercises/matrix/src/test/scala/MatrixTest.scala
@@ -1,21 +1,57 @@
-import org.scalatest.{Matchers, FlatSpec}
+import org.scalatest.{Matchers, FunSuite}
 
-/** @version created manually **/
-class MatrixTest extends FlatSpec with Matchers {
-  it should "extract a row" in {
-    Matrix("1 2\n10 20").rows(0) should be (Vector(1, 2))
-    Matrix("1 2\n10 20\n100 200").rows(2) should be (Vector(100, 200))
+/** @version 1.0.0 */
+class MatrixTest extends FunSuite with Matchers {
+
+  test("extract row from one number matrix") {
+    Matrix("1").row(0) should be(Vector(1))
   }
 
-  it should "extract a col" in {
+  test("can extract row") {
     pending
-    Matrix("1 2\n10 20").cols(0) should be (Vector(1, 10))
-    Matrix("1 2\n10 20\n100 200").cols(1) should be (Vector(2, 20, 200))
+    Matrix("1 2\n" +
+           "3 4").row(1) should be(Vector(3, 4))
   }
 
-  it should "support equality" in {
+  test("extract row where numbers have different widths") {
     pending
-    Matrix("1 2\n10 20") should be (Matrix("1 2\n10 20"))
-    Matrix("1 2\n10 20") should not be Matrix("20 10\n100 200")
+    Matrix("1 2\n" +
+           "10 20").row(1) should be(Vector(10, 20))
+  }
+
+  test("can extract row from non-square matrix") {
+    pending
+    Matrix("1 2 3\n"
+         + "4 5 6\n"
+         + "7 8 9\n"
+         + "8 7 6").row(2) should be(
+      Vector(7, 8, 9))
+  }
+
+  test("extract column from one number matrix") {
+    pending
+    Matrix("1").column(0) should be(Vector(1))
+  }
+
+  test("can extract column") {
+    pending
+    Matrix("1 2 3\n"
+         + "4 5 6\n"
+         + "7 8 9").column(2) should be(Vector(3, 6, 9))
+  }
+
+  test("can extract column from non-square matrix") {
+    pending
+    Matrix("1 2 3\n"
+         + "4 5 6\n"
+         + "7 8 9\n"
+         + "8 7 6").column(2) should be(Vector(3, 6, 9, 6))
+  }
+
+  test("extract column where numbers have different widths") {
+    pending
+    Matrix("89 1903 3\n"
+         + "18 3 1\n"
+         + "9 4 800").column(1) should be(Vector(1903, 3, 4))
   }
 }
