42f22bc81ad63b6b2771f526cc723b26c0370bca
Ric Emery <ric.emery@gmail.com>
2018-02-23 08:15:46 -0700

    Update matrix exercise to match latest canonical json.
    Add MatrixTestGenerator - this was missing from GIT.
    Rework MatrixTestGenerator to allow for better formatting of test. Though alignment of matrices is done by hand after the fact.
    
    Refs #488

---
+++ testgen/src/main/scala/MatrixTestGenerator.scala
@@ -0,0 +1,35 @@
+import java.io.File
+
+import testgen.{CanonicalDataParser, TestCaseData, TestSuiteBuilder}
+import testgen.TestSuiteBuilder.{ToTestCaseData, quote, withLabeledTest}
+
+object MatrixTestGenerator {
+  def toString(expected: CanonicalDataParser.Expected): String = {
+    expected match {
+      case Right(xs: List[Int]) => xs mkString ", "
+      case _ => throw new IllegalArgumentException
+    }
+  }
+
+  def fromLabeledTestFromInput(): ToTestCaseData =
+    withLabeledTest { sut =>
+      labeledTest =>
+        val input = labeledTest.result("input").asInstanceOf[Map[String, Any]]
+        val matrix = input("string").asInstanceOf[String].lines.map(l => s""""$l""").mkString("\\n\" +") + "\""
+        val index = input("index").toString
+        val property = labeledTest.property
+        val sutCall =
+          s"""$sut($matrix).$property($index)"""
+        val expected = s"""Vector(${toString(labeledTest.expected)})"""
+        TestCaseData(labeledTest.description, sutCall, expected)
+    }
+
+  def main(args: Array[String]): Unit = {
+    val file = new File("src/main/resources/matrix.json")
+
+    val code = TestSuiteBuilder.build(file, fromLabeledTestFromInput())
+    println(s"-------------")
+    println(code)
+    println(s"-------------")
+  }
+}
