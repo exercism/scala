57ee7180bbd43862da528f8d6704f1d54953f463
Ric Emery <ric.emery@gmail.com>
2018-03-12 21:43:53 -0700

    Update sieve exercise to match latest canonical json. Refs #488

--- testgen/src/main/scala/SieveTestGenerator.scala
+++ testgen/src/main/scala/SieveTestGenerator.scala
@@ -1,13 +1,13 @@
 import java.io.File
 
 import testgen.TestSuiteBuilder
-import testgen.TestSuiteBuilder.fromLabeledTest
+import testgen.TestSuiteBuilder.fromLabeledTestFromInput
 
 object SieveTestGenerator {
   def main(args: Array[String]): Unit = {
     val file = new File("src/main/resources/sieve.json")
 
-    val code = TestSuiteBuilder.build(file, fromLabeledTest("limit"))
+    val code = TestSuiteBuilder.build(file, fromLabeledTestFromInput("limit"))
     println(s"-------------")
     println(code)
     println(s"-------------")
