089928807f31d3e4853b08afdce11540f0bc4dfc
Ric Emery <ric.emery@gmail.com>
2018-03-15 13:09:00 -0700

    Update wordy exercise to match latest canonical json. Refs #488

--- testgen/src/main/scala/WordyTestGenerator.scala
+++ testgen/src/main/scala/WordyTestGenerator.scala
@@ -13,10 +13,10 @@ object WordyTestGenerator {
     }
   }
 
-  def fromLabeledTest(argNames: String*): ToTestCaseData =
+  def fromLabeledTestFromInput(argNames: String*): ToTestCaseData =
     withLabeledTest { sut =>
       labeledTest =>
-        val args = sutArgs(labeledTest.result, argNames: _*)
+        val args = sutArgsFromInput(labeledTest.result, argNames: _*)
         val property = labeledTest.property
         val sutCall =
           s"$sut.$property($args)"
@@ -27,7 +27,7 @@ object WordyTestGenerator {
   def main(args: Array[String]): Unit = {
     val file = new File("src/main/resources/wordy.json")
 
-    val code = TestSuiteBuilder.build(file, fromLabeledTest("input"))
+    val code = TestSuiteBuilder.build(file, fromLabeledTestFromInput("question"))
     println(s"-------------")
     println(code)
     println(s"-------------")
