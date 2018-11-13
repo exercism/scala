52e7d835345e1c8dee2fd320a76020611610b5a6
Marco Lopes <marco.lopes@ovoenergy.com>
2018-11-13 10:04:14 +0000

    Remove () from methods not returning unit on robot-simulator
    The convention in Scala is to only use () in a function/method that
    doesn't take parameters. In this case the return type is not unit, so
    the example should not have parentesis.

--- exercises/robot-simulator/example.scala
+++ exercises/robot-simulator/example.scala
@@ -4,18 +4,18 @@ case class Robot(bearing: Bearing, coordinates: (Int, Int)) {
   val x = coordinates._1
   val y = coordinates._2
 
-  def advance(): Robot = bearing match {
+  def advance: Robot = bearing match {
     case Bearing.North => Robot(bearing, (x, y + 1))
     case Bearing.South => Robot(bearing, (x, y - 1))
     case Bearing.East => Robot(bearing, (x + 1, y))
     case Bearing.West => Robot(bearing, (x - 1, y))
   }
 
-  def turnRight(): Robot =
+  def turnRight: Robot =
     if (bearing.id == Bearing.maxId - 1) Robot(Bearing(0), (x, y))
     else Robot(Bearing(bearing.id + 1), (x, y))
 
-  def turnLeft(): Robot =
+  def turnLeft: Robot =
     if (bearing.id == 0) Robot(Bearing(Bearing.maxId - 1), (x, y))
     else Robot(Bearing(bearing.id - 1), (x, y))
 
