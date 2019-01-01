ab17bcda08ebee8b8a5ea732686aa17dbb7afe7a
Ric Emery <ric.emery@gmail.com>
2019-01-01 05:58:00 -0700

    Update Acronym to match latest test data. Refs #488. (#593)
    * Update acronym to match canonical-data. Refs #488
    
    * Update acronym to match canonical-data. Refs #488

--- exercises/acronym/example.scala
+++ exercises/acronym/example.scala
@@ -1,5 +1,8 @@
 object Acronym {
   def abbreviate(phrase: String): String = {
-    "[A-Z]+[a-z]*|[a-z]+".r.findAllIn(phrase).map(s => s.head.toUpper).mkString
+    "('\\w+)|(\\w+'\\w+)|(\\w+')|(\\w+)".r
+      .findAllIn(phrase)
+      .map(_.head.toUpper)
+      .mkString
   }
 }
