39411281888090a457916663b145ece00e2dc2b4
Ric Emery <ric.emery@gmail.com>
2019-04-24 20:43:29 -0700

    Update book-store to version 1.4 of the canonical test cases

--- exercises/book-store/example.scala
+++ exercises/book-store/example.scala
@@ -1,49 +1,24 @@
 object BookStore {
-  private val bookPrice = 800
+  private val price = 8
   private val discounts =
-    Map((1, 0.0),
-      (2, 5.0),
-      (3, 10.0),
-      (4, 20.0),
-      (5, 25.0))
+    Map(0 -> 0, 1 -> 100, 2 -> 95, 3 -> 90, 4 -> 80, 5 -> 75)
+  private val cache = scala.collection.mutable.HashMap[List[Int], Int]()
 
-  def total(books: List[Int]): Double = total(books, 0)
+  private def getAllCombinations(books: List[Int]): List[List[Int]] =
+    (1 to books.size).flatMap(books.combinations).toList
 
-  private def total(books: List[Int], acc: Double): Double = {
-    if (books.isEmpty) {
-      acc
-    } else {
-      val bookNums = books.distinct
+  private def totalForCombination(books: List[Int]): Int =
+    price * books.length * discounts(books.length)
 
-      (1 to bookNums.size).foldLeft(Double.MaxValue)((minPrice, groupSize) => {
-        val itemsToRemove = bookNums.take(groupSize)
-        val remainingBooks = getRemainingBooks(books, itemsToRemove)
-        val price = total(remainingBooks, acc + groupCost(groupSize))
-        scala.math.min(price, minPrice)
-      })
-    }
+  private def getTotal(books: List[Int]): Int = books match {
+    case Nil => 0
+    case _ =>
+      getAllCombinations(books.distinct)
+        .map((combo: List[Int]) =>
+          totalForCombination(combo) + total(books.diff(combo)))
+        .min
   }
 
-  private def groupCost(groupSize: Int): Double = {
-    discounts get groupSize match {
-      case Some(discount) => bookPrice * groupSize * (100.0 - discount) / 100.0
-      case _ => bookPrice * groupSize
-    }
-  }
-
-  private def getRemainingBooks(books: List[Int], itemsToRemove: List[Int]): List[Int] = {
-    var remainingBooks = books
-
-    for (item <- itemsToRemove) {
-      remainingBooks = removeFirst(remainingBooks)(_ == item)
-    }
-
-    remainingBooks
-  }
-
-  // drops the first item in the passed in list that matches the predicate.
-  private def removeFirst[T](list: List[T])(pred: (T) => Boolean): List[T] = {
-    val (before, atAndAfter) = list span (x => !pred(x))
-    before ::: atAndAfter.drop(1)
-  }
+  def total(books: List[Int]): Int =
+    cache.getOrElseUpdate(books, getTotal(books))
 }
