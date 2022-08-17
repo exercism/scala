a9201ce00431606ff16f8f4c3093bdbdab58ca4d
Lars Westergren <lars.westergren@gmail.com>
2022-08-17 20:18:28 +0200

    Improve instructions and fix tests for PyhagoreanTriplet exercise.
    Fixes https://github.com/exercism/scala/issues/636

---
+++ exercises/practice/pythagorean-triplet/src/main/scala/PythagoreanTriplet.scala
@@ -0,0 +1,5 @@
+object PythagoreanTriplet {
+  def isPythagorean(nums: (Int, Int, Int)): Boolean = ???
+  def pythagoreanTriplets(low: Int, high:Int): Seq[(Int, Int, Int)] = ???
+  def pythagoreanTripletsSum(p: Int): Seq[(Int, Int, Int)] = ???
+}
--- exercises/practice/pythagorean-triplet/src/test/scala/PythagoreanTripletTest.scala
+++ exercises/practice/pythagorean-triplet/src/test/scala/PythagoreanTripletTest.scala
@@ -5,27 +5,33 @@ import org.scalatest.matchers.should.Matchers
 class PythagoreanTripletTest extends AnyFunSuite with Matchers {
   test("isPythagorean") {
     PythagoreanTriplet.isPythagorean((3, 4, 5)) should be (true)
-    PythagoreanTriplet.isPythagorean((3, 5, 4)) should be (true)
-    PythagoreanTriplet.isPythagorean((4, 3, 5)) should be (true)
-    PythagoreanTriplet.isPythagorean((4, 5, 3)) should be (true)
-    PythagoreanTriplet.isPythagorean((5, 3, 4)) should be (true)
-    PythagoreanTriplet.isPythagorean((5, 4, 3)) should be (true)
+    PythagoreanTriplet.isPythagorean((3, 5, 4)) should be (false)
+    PythagoreanTriplet.isPythagorean((4, 3, 5)) should be (false)
+    PythagoreanTriplet.isPythagorean((4, 5, 3)) should be (false)
+    PythagoreanTriplet.isPythagorean((5, 3, 4)) should be (false)
+    PythagoreanTriplet.isPythagorean((5, 4, 3)) should be (false)
     PythagoreanTriplet.isPythagorean((3, 3, 3)) should be (false)
     PythagoreanTriplet.isPythagorean((5, 6, 7)) should be (false)
+    PythagoreanTriplet.isPythagorean((200, 375, 425)) should be (true)
   }
 
   test("pythagoreanTriplets 1 to 10") {
-    pending
-    PythagoreanTriplet.pythagoreanTriplets(1, 10) should be (Seq((3, 4, 5), (6, 8, 10)))
+    PythagoreanTriplet.pythagoreanTriplets(1, 10) should be(Seq((3, 4, 5), (6, 8, 10)))
   }
 
   test("pythagoreanTriplets 11 to 20") {
-    pending
-    PythagoreanTriplet.pythagoreanTriplets(11, 20) should be (Seq((12, 16, 20)))
+    PythagoreanTriplet.pythagoreanTriplets(11, 20) should be(Seq((12, 16, 20)))
   }
 
   test("pythagoreanTriplets 56 to 95") {
-    pending
-    PythagoreanTriplet.pythagoreanTriplets(56, 95) should be (Seq((57, 76, 95), (60, 63, 87)))
+    PythagoreanTriplet.pythagoreanTriplets(56, 95) should be(Seq((57, 76, 95), (60, 63, 87)))
+  }
+
+  test("pythagoreanTripletsSum N=180") {
+    PythagoreanTriplet.pythagoreanTripletsSum(180) should be(Seq((18, 80, 82), (30, 72, 78), (45, 60, 75)))
+  }
+
+  test("pythagoreanTripletsSum N=1000") {
+    PythagoreanTriplet.pythagoreanTripletsSum(1000) should be(Seq((200, 375, 425)))
   }
 }
