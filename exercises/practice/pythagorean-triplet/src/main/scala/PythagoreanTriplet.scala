d05f984435aeff58efd1371c31908f890758ff9f
Lars Westergren <lars.westergren@gmail.com>
2022-08-27 12:24:50 +0200

    Reverting changes to PythagoreanTriplet.scala, the change would break all existing submissions

--- exercises/practice/pythagorean-triplet/src/main/scala/PythagoreanTriplet.scala
+++
@@ -1,5 +0,0 @@
-object PythagoreanTriplet {
-  def isPythagorean(nums: (Int, Int, Int)): Boolean = ???
-  def pythagoreanTriplets(low: Int, high:Int): Seq[(Int, Int, Int)] = ???
-  def pythagoreanTripletsSum(p: Int): Seq[(Int, Int, Int)] = ???
-}
