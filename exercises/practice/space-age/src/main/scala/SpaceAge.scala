cce7b3d4d9d67737f17edf84cc91df4a1c64342c
jabertuhin <jabertuhin@gmail.com>
2022-11-17 16:04:15 +0600

    Add SpaceAge skeleton class. (#740)
    * Add SpaceAge class.
    
    * Update exercises/practice/space-age/src/main/scala/SpaceAge.scala
    
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>

---
+++ exercises/practice/space-age/src/main/scala/SpaceAge.scala
@@ -0,0 +1,17 @@
+object SpaceAge {
+  def onEarth(age: Double): Double = ???
+
+  def onVenus(age: Double): Double = ???
+
+  def onMercury(age: Double): Double = ???
+
+  def onMars(age: Double): Double = ???
+
+  def onJupiter(age: Double): Double = ???
+
+  def onSaturn(age: Double): Double = ???
+
+  def onUranus(age: Double): Double = ???
+
+  def onNeptune(age: Double): Double = ???
+}
