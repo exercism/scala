5b7669c131ff4ae0873570f02539f9d0a86850bb
Victor Osipov <osipovvictor1994@gmail.com>
2023-05-05 16:24:40 +0100

    Fix RobotNameTest.scala perf test

--- exercises/practice/robot-name/src/test/scala/RobotNameTest.scala
+++ exercises/practice/robot-name/src/test/scala/RobotNameTest.scala
@@ -35,11 +35,11 @@ class RobotNameTest extends AnyFunSpec with Matchers {
 
   // Making this test pass is an optional extra exercise, should you want more of a challenge.
   // It's ignored by default, to make it run, simply change "ignore" below to "it".
-  // There are 26^2 * 1,000 = 676,000 possible robot names - you have to ensure that none are repeated.
-  // The Robot code needs to be efficient enough to allow all 676,000 unique names to be generated.
+  // There are 26* 26 * 900 = 608,400 possible robot names - you have to ensure that none are repeated.
+  // The Robot code needs to be efficient enough to allow all 608,400 unique names to be generated.
   ignore("a large number of new instances have unique names") {
     val alreadySet = mutable.HashSet.empty[String]
-    for(_ <- 0 until 676000 - 6) { // as 6 robot names are generated in the tests above!!
+    for(_ <- 0 until 608400 - 6) { // as 6 robot names are generated in the tests above!!
       val name = new Robot().name
       if (alreadySet contains name) {
         fail(s"$name is repeated")
