067808f9c579cec829761aab5377aa27caa70f84
Abhijit Sarkar <contact@asarkar.com>
2024-09-06 16:30:13 -0700

    Remove redundant definition of day of week in favor of java.time.DayOfWeek

--- exercises/practice/meetup/src/main/scala/Meetup.scala
+++ exercises/practice/meetup/src/main/scala/Meetup.scala
@@ -4,20 +4,9 @@ import Schedule.Schedule
 
 case class Meetup(month: Int, year: Int) {
 
-  def day(dayOfWeek: Int, schedule: Schedule): LocalDate = ???
+  def day(dayOfWeek: DayOfWeek, schedule: Schedule): LocalDate = ???
 }
 
-object Schedule extends Enumeration {
-  type Schedule = Value
-  val Teenth, First, Second, Third, Fourth, Last = Value
-}
-
-object Meetup {
-  val Mon = DayOfWeek.MONDAY.getValue
-  val Tue = DayOfWeek.TUESDAY.getValue
-  val Wed = DayOfWeek.WEDNESDAY.getValue
-  val Thu = DayOfWeek.THURSDAY.getValue
-  val Fri = DayOfWeek.FRIDAY.getValue
-  val Sat = DayOfWeek.SATURDAY.getValue
-  val Sun = DayOfWeek.SUNDAY.getValue
+enum Schedule {
+  case First, Second, Third, Fourth, Last, Teenth
 }
